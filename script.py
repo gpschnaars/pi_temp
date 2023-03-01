996f82a2be7f72a9e78bc055286d7f9bc4629059
gpschnaars@gmail.com <gpschnaars@gmail.com>
2023-03-01 00:07:03 -0500

    initial commit

---
+++ script.py
@@ -0,0 +1,41 @@
+import time
+import board
+from adafruit_dht import DHT22
+
+
+
+
+def main():
+	device = DHT22(board.D4, use_pulseio = False)
+
+
+
+	while True:
+
+		try:
+			temp, humi = device.temperature, device.humidity
+			print(
+				f"{time.ctime()} ... ... Temp: {temp} F,  Humidity: {humi} %"
+		)
+
+		except RuntimeError as error:
+			print(error.args[0])
+			time.sleep(10)
+			continue
+
+		except Exception as error:
+			dhtDevice.exit()
+			raise error
+
+		except KeyboardInterrupt:
+			dhtDevice.exit()
+			print("exiting ... ")
+			break
+
+		finally:
+			time.sleep(10)
+
+
+if __name__ == '__main__':
+
+	main()
